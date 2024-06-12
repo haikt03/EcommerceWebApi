ac1995e3a2f3f2c7669a3479dba4b7a0281c8502
haikt03 <tuanhai1652003@gmail.com>
2024-06-12 22:10:36 +0700

    Create react basics

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,11 +11,11 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductController : ControllerBase
+    public class ProductsController : ControllerBase
     {
         private readonly StoreContext _context;
 
-        public ProductController(StoreContext context)
+        public ProductsController(StoreContext context)
         {
             _context = context;
         }
